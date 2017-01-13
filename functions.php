f03af53d246a6750d4ec241f8d7a0ece91261e0c
Imran Mondal <hello@imranmondal.com>
2017-01-13 21:15:35 +0600

    Function.php updated & Custom style added

--- functions.php
+++ functions.php
@@ -1,7 +1,10 @@
 <?php
+
+/**
+ * Importing Parent Themes Styles
+ */
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 function my_theme_enqueue_styles() {
-    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 
 }
-?>
